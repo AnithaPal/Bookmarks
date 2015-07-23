b95215e0f43c61b33ca95dd4fd4412ead208836b
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-23 07:11:16 -0400

    user spec added

--- config/application.rb
+++ config/application.rb
@@ -38,8 +38,8 @@ module Bookmarks
             helper_specs: false,
             routing_specs: false,
             controller_specs: true,
-            request_specs: false,
-            g.fixture_replacement :factory_girl, dir: "spec/factories"
+            request_specs: false
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
     end        
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
-require 'rails_helper'
+  require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe User do
+    it "is  valid with name, email and password" do
+      user = User.new(
+        name: 'User1',
+        email: 'user1@example.com',
+        password: 'helloworld')
+      expect(user).to be_valid?
+    end  
+    it "is invalid without a name" do
+      user = User.new(name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
+    end  
+    it "is invalid without an email" do
+      user = User.new(email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end  
+    it "is invalid without a password" do
+      user = User.new(password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end  
+
+    it "is invalid with duplicate email address" do
+      User.create(
+        name: 'user2',
+        email: 'user2@example.com',
+        password: 'helloworld')
+
+      user = User.new(
+        name: 'user',
+        email: 'user2@example.com',
+        password: 'helloworld')
+      user.valid?
+      expect(user.errors[:email]).to include("has already been taken")
+    end  
+  end  
 end
