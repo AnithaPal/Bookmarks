1691c4f2219a11d577b90a6ba76d297631424edf
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-28 09:50:15 -0400

    mailgun configured, sendgrid removed and incoming controller is in process

---
+++ app/controllers/incoming_controller.rb
@@ -0,0 +1,12 @@
+class IncomingController < ApplicationController
+  skip_before_action :verify_authenticity_token, only : [:create]
+
+  def create
+     puts "INCOMING PARAMS HERE: #{params}"
+     @user = User.find_by(email: params[:sender])
+     @topic = Topic.find_by(title: params[:subject])
+     @url = params["body_plain"]
+
+  end   
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  
+  post :incoming, to: 'incoming#create'
 
   get 'welcome/about'
 
