b7f042a658efd5c816e98870fda3db9b9a375b83
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-29 06:04:57 -0400

    topics controller added

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,57 @@
+class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topics_params)
+    if @topic.save
+      flash[:notice] = "Topic was created successfully"
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error creating your topic. Please try again"
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+    if @topic.update_attributes(topics_params)
+      flash[:notice] = "Topics was updated successfully"
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error updating your topic. Please try again"
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(Params[:id])
+    if @topic.destroy
+      flash[:notice] = "Topic was successfully deleted"
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting your topic. Please try again."
+    end
+  end
+
+
+  private
+
+  def topics_params
+    params.require(:topic).permit(:title)
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
   post :incoming, to: 'incoming#create'
+
+  resources :topics
 
   get 'welcome/about'
 
