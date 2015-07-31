38d3899d683316a09cdc38e54b962823de4509a7
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-31 09:45:40 -0400

    like model created

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
+  has_many :lkes, dependent: :destroy
 
   validates :url, presence: true
   validates :url, format: { with: URI::regexp(%w(http https)), message: "This is an invalid URL. It should start with http or https"}
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
-  has_many :topics
+  has_many :topics dependent: :destroy
+  has_many :bookmarks dependent: :destroy
+  has_many :likes dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
+
+  def liked(bookmark)
+    likes.where(bookmark_id: bookmark.id).first
+  end
+
 end
