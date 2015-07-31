d8510e6102320d9dc199e6f3240bed2fc40bdf7d
AnithaPal <anitha.aravindvel@gmail.com>
2015-07-31 11:54:31 -0400

    topic policy error fix

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,5 @@ class Topic < ActiveRecord::Base
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
   validates :title, presence: true
+  validates :title, uniqueness: {scope: :user_id}
 end
