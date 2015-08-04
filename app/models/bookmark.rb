1b9d02e343e58eb0f6baf04e0f5981e725c6f212
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-04 08:14:35 -0400

    image uploader added

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,6 +3,8 @@ class Bookmark < ActiveRecord::Base
   belongs_to :user
   has_many :lkes, dependent: :destroy
 
+  mount_uploader :image, BookmarkImageUploader
+
   validates :url, presence: true
   validates :url, format: { with: URI::regexp(%w(http https)), message: "This is an invalid URL. It should start with http or https"}
  end 
