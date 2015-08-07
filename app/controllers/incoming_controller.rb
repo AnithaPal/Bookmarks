297d598df3b7afc0e6028cbe181da62d78bacdc5
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-07 10:49:06 -0400

    incoming controller bug fix

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,10 +4,10 @@ class IncomingController < ApplicationController
   def create
     @user = User.find_by(email: params[:sender])
     @topic = Topic.find_by(title: params[:subject])
-    @url = params["body_plain"]
+    @url = params["stripped-text"]
 
     if @user.nil?
-      @user = User.new(email: params[:sender], password: "temp0rary_passw0rd")
+      @user = User.new(email: params[:sender], name: "Guest", password: "temp0rary_passw0rd")
       @user.skip_confirmation!
       @user.save!
      end
@@ -16,7 +16,7 @@ class IncomingController < ApplicationController
       @topic = @user.topics.create(title: params[:subject])
     end
 
-    @bookmark = @topic.bookmarks.create(url: @url)
+    @bookmark = @topic.bookmarks.create!(url: @url, user: @user)
 
     head 200
   end
